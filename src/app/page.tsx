4cf5d040fb8f337d119c2df3b392375aef28e4c5
Peep Pirnpuu <peep@pirnpuu.ee>
2024-09-17 10:59:53 +0300

    Subscribe to real-time BTC updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
-export default function Home() {
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+const BinanceWebSocket = () => {
+  const [btcPrice, setBtcPrice] = useState<number>(0);
+
+  useEffect(() => {
+    // Subscribe to stream of trades to get real-time overview of the BTC price in USD
+    const ws = new WebSocket("wss://stream.binance.com:9443/ws/btcusdt@trade");
+
+    ws.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      setBtcPrice(parseFloat(data.p)); // p is the price in the trade message
+    };
+
+    return () => ws.close();
+  }, []);
+
   return (
-    <main>
-      <div>Hello world!</div>
-    </main>
+    <div>
+      <h1>BTC Price from Binance WebSocket</h1>
+      {btcPrice ? <p>{btcPrice} USD</p> : <p>Fetching price...</p>}
+    </div>
   );
-}
+};
+
+export default BinanceWebSocket;
